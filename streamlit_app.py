6b1b8cdeb4bfd94cfdc3f3d35f69635ee42f6b7b
Le0nard28 <162983428+Le0nard28@users.noreply.github.com>
2024-03-18 11:59:03 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vREj6UjW5ykyL4n89OuOlbIu0S5AVUuC7Y9aDG1tDrJerg7FusLqfrR7XLVwxFqTehJlbktv0r-JzCJ/pub?output=csv')
+st.dataforme(voc)
+l=voc.sharpe[o]
+i=np.random.choice(range(l))
+word_fr=voc['Definition'].values[i]
+word_chi=voc['hanzi']
+sti.write(word_fr+"hanzi"+wrd_chi)
