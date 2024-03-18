7f1352bf84d8f699d9994c604298286d47e07293
Le0nard28 <162983428+Le0nard28@users.noreply.github.com>
2024-03-18 12:31:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import pandas as pd
 import numpy as np
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vREj6UjW5ykyL4n89OuOlbIu0S5AVUuC7Y9aDG1tDrJerg7FusLqfrR7XLVwxFqTehJlbktv0r-JzCJ/pub?output=csv')
 st.dataframe(voc)
-
+l=voc.shape[0]
