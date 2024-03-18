884f8e57cf798e3a8832bf8dce10190fcf4ed026
Le0nard28 <162983428+Le0nard28@users.noreply.github.com>
2024-03-18 12:07:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vREj6UjW5ykyL4n89OuOlbIu0S5AVUuC7Y9aDG1tDrJerg7FusLqfrR7XLVwxFqTehJlbktv0r-JzCJ/pub?output=csv')
-l=voc.sharpe[o]
+l=voc.sharpe[0]
 i=np.random.choice(range(l))
 word_fr=voc['Definition'].values[i]
 word_chi=voc['hanzi'].values[i]
